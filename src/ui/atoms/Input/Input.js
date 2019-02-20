27e078e992d0b1e61d8b616a6863da0742e58b87
msal96 <mihailsalari1996@yahoo.com>
2019-02-20 17:35:17 +0200

    refactor(Input.js): refactor Input using styled-system

--- src/ui/atoms/Input/Input.js
+++ src/ui/atoms/Input/Input.js
@@ -1,50 +1,65 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import {
+  borders,
+  borderRadius,
+  minHeight,
+  maxHeight,
+  minWidth,
+  maxWidth,
+  position,
+  size,
+  space
+} from 'styled-system'
+
+import theme from 'assets/theme'
 
 const Input = ({
   autoComplete,
-  border,
-  borderRadius,
   dataTest,
   disabled,
   error,
-  height,
-  marginBlockStart,
+  blockSize,
+  inlineSize,
   onChange,
-  padding,
   placeholder,
   required,
   type,
   value,
-  width
+  theme,
+  ...rest
 }) => (
   <StyledInput
     autoComplete={autoComplete}
-    border={border}
     borderRadius={borderRadius}
     data-testid={dataTest}
     disabled={disabled}
     error={error}
-    height={height}
-    marginBlockStart={marginBlockStart}
+    blockSize={blockSize}
     onChange={onChange}
-    padding={padding}
     placeholder={placeholder}
     required={required}
     type={type}
     value={value}
-    width={width}
+    inlineSize={inlineSize}
+    theme={theme}
+    {...rest}
   />
 )
 
 const StyledInput = styled.input`
-  border: ${props => props.border}px solid ${props => (props.error ? 'red' : '#c3c3c3')};
-  border-radius: ${props => props.borderRadius}px;
-  height: ${props => props.height}px;
-  margin-block-start: ${props => props.marginBlockStart}px;
-  padding: ${props => props.padding}px;
-  width: ${props => props.width}px;
+  ${borderRadius};
+  ${borders};
+  ${minHeight};
+  ${maxHeight};
+  ${minWidth};
+  ${maxWidth};
+  ${position};
+  ${size};
+  ${space};
+  block-size: ${props => props.blockSize};
+  inline-size: ${props => props.inlineSize};
   :focus {
     outline-style: none;
     outline-color: transparent;
@@ -56,15 +71,10 @@ const StyledInput = styled.input`
 
 Input.propTypes = {
   autoComplete: PropTypes.string,
-  border: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  borderRadius: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   dataTest: PropTypes.string,
   disabled: PropTypes.bool,
   error: PropTypes.bool,
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  marginBlockStart: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   onChange: PropTypes.func.isRequired,
-  padding: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   placeholder: PropTypes.string,
   required: PropTypes.bool,
   type: PropTypes.string.isRequired,
@@ -73,16 +83,20 @@ Input.propTypes = {
   PropTypes.number,
   PropTypes.object,
   ]),
-  width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  ...minHeight.propTypes,
+  ...maxHeight.propTypes,
+  ...minWidth.propTypes,
+  ...maxWidth.propTypes,
+  ...space.propTypes,
+  ...size.propTypes
 }
 
 Input.defaultProps = {
   disabled: false,
-  height: 20,
   placeholder: 'type...',
   required: false,
   type: 'text',
-  width: 150
+  theme
 }
 
 export default Input
