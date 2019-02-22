93c6b81b1e3019702c8088349f0640cc8d9532e1
msal96 <mihailsalari1996@yahoo.com>
2019-02-22 11:11:46 +0200

    feat(Header): split Header in 3 sections, add functionality to style each section, add examples

--- src/ui/molecules/Header/Header.js
+++ src/ui/molecules/Header/Header.js
@@ -5,8 +5,8 @@ import styled from 'styled-components'
 import {
   alignContent,
   alignItems,
+  alignSelf,
   color,
-  display,
   height,
   justifyContent,
   maxHeight,
@@ -17,20 +17,65 @@ import {
   space
 } from 'styled-system'
 
-const Header = ({ left, middle, right, ...rest }) => (
+const Header = ({
+  left,
+  middle,
+  right,
+  justifyLeft,
+  justifyMiddle,
+  justifyRight,
+  flexDirectionLeft,
+  flexDirectionMiddle,
+  flexDirectionRight,
+  alignItemsLeft,
+  alignItemsMiddle,
+  alignItemsRight,
+  ...rest
+}) => (
   <StyledHeader {...rest}>
-    {left ? <LeftSection>{left}</LeftSection> : null}
-    {middle ? <CentralSection>{middle}</CentralSection> : null}
-    {right ? <RightSection>{right}</RightSection> : null}
+    { left ? 
+      <LeftSection
+        justifyLeft={justifyLeft}
+        flexDirectionLeft={flexDirectionLeft}
+        alignItemsLeft={alignItemsLeft}
+      >
+        {left}
+      </LeftSection>
+        : <LeftSection />
+    }
+    { middle ? 
+      <MiddleSection
+        justifyMiddle={justifyMiddle}
+        flexDirectionMiddle={flexDirectionMiddle}
+        alignItemsMiddle={alignItemsMiddle}
+      >
+        {middle}
+      </MiddleSection>
+        : <MiddleSection />
+    }
+    { right ? 
+      <RightSection
+        justifyRight={justifyRight}
+        flexDirectionRight={flexDirectionRight}
+        alignItemsRight
+      >
+        {right}
+      </RightSection>
+        : <RightSection />
+    }
   </StyledHeader>
 )
 
 const StyledHeader = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  align-self: center;
   ${alignContent}
   ${alignItems}
+  ${alignSelf}
   ${color}
-  ${display}
   ${height}
   ${justifyContent}
   ${maxHeight}
@@ -42,23 +87,38 @@ const StyledHeader = styled.div`
 `
 
 const LeftSection = styled.div`
-  position: absolute;
-  width: 33.33%;
+  display: flex;
+  flex: 1;
+  justify-content: ${props =>
+    props.justifyLeft ? props.justifyLeft : 'center'};
+  flex-direction: ${props => props.flexDirectionLeft ? props.flexDirectionLeft : 'row'};
+  align-items: ${props => props.alignItemsLeft ? props.alignItemsLeft : 'center'};
+  align-content: center;
+  flex-wrap: wrap;
 `
-const CentralSection = styled.div`
-  position: absolute;
-  width: 33.33%;
+const MiddleSection = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: ${props => props.justifyMiddle ? props.justifyMiddle : 'center'};
+  flex-direction: ${props => props.flexDirectionMiddle ? props.flexDirectionMiddle : 'row'};
+  align-items: ${props => props.alignItemsMiddle ? props.alignItemsMiddle : 'center'};
+  align-self: center;
+  flex-wrap: wrap;
 `
 const RightSection = styled.div`
-  position: absolute;
-  width: 33.33%;
+  display: flex;
+  flex: 1;
+  justify-content: ${props => props.justifyRight ? props.justifyRight : 'center'};
+  flex-direction: ${props => props.flexDirectionRight ? props.flexDirectionRight : 'row'};
+  align-items: ${props => props.alignItemsRight ? props.alignItemsRight : 'center'};
+  flex-wrap: wrap;
 `
 
 Header.propTypes = {
   ...alignContent.propTypes,
   ...alignItems.propTypes,
+  ...alignSelf.propTypes,
   ...color.propTypes,
-  ...display.propTypes,
   ...height.propTypes,
   ...justifyContent.propTypes,
   ...maxHeight.propTypes,
@@ -79,6 +139,15 @@ Header.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
   ]),
+  justifyLeft: PropTypes.string,
+  justifyMiddle: PropTypes.string,
+  justifyRight: PropTypes.string,
+  flexDirectionLeft: PropTypes.string,
+  flexDirectionMiddle: PropTypes.string,
+  flexDirectionRight: PropTypes.string,
+  alignItemsLeft: PropTypes.string,
+  alignItemsMiddle: PropTypes.string,
+  alignItemsRight: PropTypes.string
 }
 
 export default Header
