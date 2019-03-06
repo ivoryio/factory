f8f3d356c94375560cbea2a65bb39bc2c44c5942
msal96 <mihailsalari1996@yahoo.com>
2019-03-06 09:15:47 +0200

    feat(MenuList): Add MenuList with Example

--- packages/kogaio/src/lib/MenuList/MenuList.atom.js
+++ packages/kogaio/src/lib/MenuList/MenuList.atom.js
@@ -1,156 +1,126 @@
-// import React, { useState } from 'react'
-// import PropTypes from 'prop-types'
-// import styled from 'styled-components'
-// import {
-//   alignItems,
-//   alignSelf,
-//   borderColor,
-//   borderRadius,
-//   borders,
-//   bottom,
-//   color,
-//   display,
-//   flex,
-//   flexBasis,
-//   justifyContent,
-//   left,
-//   maxHeight,
-//   maxWidth,
-//   minHeight,
-//   minWidth,
-//   opacity,
-//   overflow,
-//   position,
-//   right,
-//   themeGet,
-//   top,
-//   space
-// } from 'styled-system'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import {
+  alignItems,
+  alignSelf,
+  borderColor,
+  borderRadius,
+  borders,
+  bottom,
+  color,
+  display,
+  flex,
+  flexBasis,
+  justifyContent,
+  left,
+  maxHeight,
+  maxWidth,
+  minHeight,
+  minWidth,
+  opacity,
+  overflow,
+  position,
+  right,
+  themeGet,
+  top,
+  space
+} from 'styled-system'
 
-// import { Box } from '../Responsive'
-// import Text from '../Text'
-// import Touchable from '../Touchable'
+import { Box } from '../Responsive'
+import Text from '../Text'
+import Touchable from '../Touchable'
 
-// const Dropdown = ({
-//   onChangeOption,
-//   options,
-//   placeholder,
-//   selectedOption,
-//   ...rest
-// }) => {
-//   const [showList, setShowList] = useState(false)
-//   const handleShowListChange = () => setShowList(!showList)
-//   const selectOption = option => () => {
-//     onChangeOption(option)
-//     handleShowListChange()
-//   }
-//   return (
-//     <Container {...rest}>
-//       <StyledTouchable onClick={handleShowListChange}>
-//         <Text fontFamily='Roboto' fontSize='1em'>
-//           {selectedOption || placeholder}
-//         </Text>
-//       </StyledTouchable>
-//       {showList && (
-//         <ListWrapper>
-//           {options.map(option => (
-//             <OptionWrapper key={option.key}>
-//               <TouchableText
-//                 onClick={selectOption(option.name)}
-//               >
-//                 <Text fontFamily='Roboto' fontSize='1em'>
-//                   {option.name}
-//                 </Text>
-//               </TouchableText>
-//             </OptionWrapper>
-//           ))}
-//         </ListWrapper>
-//       )}
-//     </Container>
-//   )
-// }
+const MenuList = ({
+  listItems,
+  onSelectItem,
+  ...rest
+}) => (
+  <Container {...rest}>
+    <ListWrapper>
+      {listItems.map(item => (
+        <TouchableText
+          key={item.key}
+          onClick={onSelectItem(item.name)}
+        >
+          <Text mx={3} color='dark-gunmetal' fontFamily='Roboto' fontSize='1em' textAlign='left'>
+            {item.name}
+          </Text>
+        </TouchableText>
+      ))}
+    </ListWrapper>
+  </Container>
+)
 
-// const Container = styled(Box)`
-//   display: flex;
-//   flex-direction: column;
-//   border-radius: 2px;
-//   box-shadow: 0 1px 4px 0 rgba(22, 29, 37, 0.35);
-//   position: relative;
-//   &:after {
-//     box-shadow: 1px -1px 1px 0 rgba(22, 29, 37, 0.35);
-//     background-color: ${themeGet('colors.white')};
-//     content: '';
-//     display: block;
-//     position: absolute;
-//     width: 8px;
-//     height: 8px;
-//     top: -3px;
-//     right: 25px;
-//     -moz-transform: rotate(-45deg);
-//     -webkit-transform: rotate(-45deg);
-//   }
-//   ${alignItems}
-//   ${alignSelf}
-//   ${borderColor}
-//   ${borderRadius}
-//   ${borders}
-//   ${bottom}
-//   ${color}
-//   ${display}
-//   ${flex}
-//   ${flexBasis}
-//   ${justifyContent}
-//   ${left}
-//   ${maxHeight}
-//   ${maxWidth}
-//   ${minHeight}
-//   ${minWidth}
-//   ${opacity}
-//   ${overflow}
-//   ${position}
-//   ${right}
-//   ${space}
-//   ${top}
-// `
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 2px;
+  box-shadow: 0 1px 4px 0 rgba(22, 29, 37, 0.35);
+  position: relative;
+  &:after {
+    box-shadow: 1px -1px 1px 0 rgba(22, 29, 37, 0.35);
+    background-color: ${themeGet('colors.white')};
+    content: '';
+    display: block;
+    position: absolute;
+    width: 8px;
+    height: 8px;
+    top: -4px;
+    right: 25px;
+    -moz-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+  }
+  ${alignItems}
+  ${alignSelf}
+  ${borderColor}
+  ${borderRadius}
+  ${borders}
+  ${bottom}
+  ${color}
+  ${display}
+  ${flex}
+  ${flexBasis}
+  ${justifyContent}
+  ${left}
+  ${maxHeight}
+  ${maxWidth}
+  ${minHeight}
+  ${minWidth}
+  ${opacity}
+  ${overflow}
+  ${position}
+  ${right}
+  ${space}
+  ${top}
+`
 
-// const StyledTouchable = styled(Touchable)`
-//   background-color: ${themeGet('colors.white')};
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-// `
-// const ListWrapper = styled.div`
-//   align-items: center;
-//   background-color: ${themeGet('colors.white')};
-//   display: flex;
-//   flex-direction: column;
-//   & > :nth-child(n + 2) {
-//     border-top: 1px solid ${themeGet('colors.azureish-white')};
-//   }
-// `
-// const OptionWrapper = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   width: 100%;
-// `
-// const TouchableText = styled(Touchable)`
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: center;
-//   padding-bottom: ${themeGet('space.1')}px;
-//   padding-top: ${themeGet('space.1')}px;
-//   width: 85%;
-// `
+const ListWrapper = styled.div`
+  align-items: center;
+  background-color: ${themeGet('colors.white')};
+  display: flex;
+  flex-direction: column;
+  & > :nth-child(n + 2) {
+    border-top: 1px solid ${themeGet('colors.azureish-white')};
+  }
+`
 
-// Dropdown.propTypes = {
-//   options: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   placeholder: PropTypes.string,
-//   selectedOption: PropTypes.string,
-//   onChangeOption: PropTypes.func.isRequired
-// }
+const TouchableText = styled(Touchable)`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  padding-bottom: ${themeGet('space.3')}px;
+  padding-top: ${themeGet('space.3')}px;
+  width: 100%;
+  z-index: 99;
+  :hover {
+    background-color: ${themeGet('colors.white-smoke')}
+  }
+`
 
-// Dropdown.defaultProps = {
-//   placeholder: 'Pick one option, please'
-// }
-// export default Dropdown
+MenuList.propTypes = {
+  listItems: PropTypes.arrayOf(PropTypes.object).isRequired,
+  onSelectItem: PropTypes.func.isRequired
+}
+
+export default MenuList
--- packages/kogaio/src/lib/MenuList/MenuList.examples.md
+++ packages/kogaio/src/lib/MenuList/MenuList.examples.md
@@ -1,39 +1,58 @@
 #### Simple Menu List
-<!-- ```js
+```js
 const React = require('react')
 const { useState } = React
-const DropdownExample = () => {
-const [selectedOption, setSelectedOption] = useState('')
-const onChangeOption = newOption =>
-  setSelectedOption(newOption)
-
-const options = [
+const { default: Icon } = require('../Icon')
+const MenuListExample = () => {
+const [selectedItem, setSelectedItem] = useState('')
+const [showList, setShowList] = useState(false)
+const onChangeItem = item =>
+  setSelectedOption(item)
+const onChangeShowList = () =>
+  setShowList(!showList)
+const selectItem = item => () => {
+    onChangeItem(item)
+    onChangeShowList()
+  }
+const listItems = [
   {
-    key: 'option1',
-    name: 'First Option'
+    key: 'list-item1',
+    name: 'Menu list item 1'
   },
   {
-    key: 'option2',
-    name: 'Second Option'
+    key: 'list-item2',
+    name: 'Menu list item 2'
   },
   {
-    key: 'option3',
-    name: 'Third Option'
+    key: 'list-item3',
+    name: 'Menu list item 3'
   }
 ]
+const menuListWrapper = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
+}
   return (
-      <Dropdown
-        mx='auto'
+    <div style={menuListWrapper}>
+      <Icon
+        cursor='pointer'
+        name='notification_important'
+        onClick={onChangeShowList}
+        fontSize='2rem'
+      />
+      { showList &&
+      <MenuList
+        mt={2}
         fontSize='1rem'
         fontFamily='Roboto, sans-serif, -apple-system, BlinkMacSystemFont'
-        onChangeOption={onChangeOption}
-        options={options}
-        placeholder='Please pick your option...'
-        py={12}
-        selectedOption={selectedOption}
+        onSelectItem={selectItem}
+        listItems={listItems}
         width={{ xs: 1/2, md: 1/3, lg: 1/4}}
       />
+      }
+    </div>
   )
 }
-  <DropdownExample />
-``` -->
+  <MenuListExample />
+```
