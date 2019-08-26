37cfbe1a47fc19db3fa351ce74cb08fa512dc5b5
Mihai <mihai.rosca@thinslices.com>
2019-08-26 10:33:38 +0300

    style(tooltip): adjust tooltip style and structure

--- packages/kogaio/src/lib/assets/variants/tooltips.js
+++ packages/kogaio/src/lib/assets/variants/tooltips.js
@@ -1,41 +1,46 @@
-export default function ({ borders, colors, ...theme }) {
+export default function({ borders, colors, ...theme }) {
   return {
-    info: {
-      border: `${borders[1]} ${colors.info}`,
+    default: {
       'background-color': colors.white,
-      '& > div': {
-        'i:first-of-type, .tooltip-text': {
-          color: colors.info
-        }
+      border: `${borders[1]} ${colors.independence}`,
+      'i:first-of-type, .tooltip-text': {
+        color: colors.gunmetal
       },
       ':after': {
-        borderColor: colors.info,
+        'border-color': colors.independence,
+        'background-color': colors.white
+      }
+    },
+    info: {
+      'background-color': colors.white,
+      border: `${borders[1]} ${colors.info}`,
+      'i:first-of-type, .tooltip-text': {
+        color: colors.info
+      },
+      ':after': {
+        'border-color': colors.info,
         'background-color': colors.white
       }
     },
     success: {
-      border: `${borders[1]} ${colors.brand}`,
       'background-color': colors.white,
-      '& > div': {
-        'i:first-of-type, .tooltip-text': {
-          color: colors.brand
-        }
+      border: `${borders[1]} ${colors.brand}`,
+      'i:first-of-type, .tooltip-text': {
+        color: colors.success
       },
       ':after': {
-        borderColor: colors.brand,
+        'border-color': colors.success,
         'background-color': colors.white
       }
     },
     error: {
-      border: `${borders[1]} ${colors.error}`,
       'background-color': colors.white,
-      '& > div': {
-        'i:first-of-type, .tooltip-text': {
-          color: colors.error
-        }
+      border: `${borders[1]} ${colors.error}`,
+      'i:first-of-type, .tooltip-text': {
+        color: colors.error
       },
       ':after': {
-        borderColor: colors.error,
+        'border-color': colors.error,
         'background-color': colors.white
       }
     }
